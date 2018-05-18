fc4c683d21d69fb23bd88741000cc999620bee8e
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-05-18 10:18:03 +0200

    removed gitignore for connect.php and added it without user and password

---
+++ src/php/connect.php
@@ -0,0 +1,32 @@
+<?php
+/* code by x4m3 <https://philippeloctaux.com> */
+/* source on github */
+
+/******************** connect to the mysql server and get stuff from it */
+
+/* host; database name; user; password; */
+try {
+  $bdd = new PDO('mysql:host=localhost;dbname=poubelle', 'user', 'password');
+}
+
+/* if there's an error, we can go fuck ourselves */
+catch(PDOException $e) {
+  echo $e->getMessage();
+  die("fuck off");
+}
+
+/* select the table */
+$reponse = $bdd->query('SELECT * FROM masse ORDER BY id DESC LIMIT 0, 1');
+
+/* organize shit */
+$donnees = $reponse->fetch();
+
+/* save shit from database to the php vars */
+$id = $donnees['id'];
+$matiere = $donnees['matiere'];
+$masse = $donnees['masse'];
+$time = $donnees['time'];
+
+/* close the mysql connection */
+$reponse->closeCursor();
+?>
